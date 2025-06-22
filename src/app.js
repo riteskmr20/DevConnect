7454030b62e80afa4270753d15a3241603985ccf
Ritesh kumar <riteshkmr2002@gmail.com>
2025-06-22 22:03:55 +0530

    platying with the routes

--- src/app.js
+++ src/app.js
@@ -5,43 +5,15 @@ const app=express();
 
 
 
-
-app.use("/user",(req,res)=>{
-   res.send("HAHAHAHAHA");
-});
-
-
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Ritesh",lastname:"Kumar"});
-})
-
-app.post("/user",(req,res)=>{
-    res.send("Data successfully saved to the database!");
-})
-
-
-app.delete("/user",(req,res)=>{
-    res.send("Data sucessfully deleted from the database!");
-});
-
-
- app.use("/hello/2",(req,res)=>{
-    res.send("Hello,Hello,Hello");
-});
-
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the server");
-});
  
-
-
-app.use("/",(req,res)=>{
-    res.send("Namaste Ritesh");
+app.get("/user/:userId/:name/:password",(req,res)=>{ 
+    console.log(req.params);
+    res.send({firstname:"Ritesh",lastname:"Kumar"});
 });
 
 
+      
 
-     
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000");
 });
