4679c2eadd6cc5b5483e91c8c5fde3638bb51ad6
Ritesh kumar <riteshkmr2002@gmail.com>
2025-06-22 14:59:31 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -3,9 +3,44 @@ const express=require("express");
 const app=express();
 
 
-app.use((req,res)=>{
-    res.send("Hello from the server");
+
+
+
+app.use("/user",(req,res)=>{
+   res.send("HAHAHAHAHA");
+});
+
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Ritesh",lastname:"Kumar"});
 })
+
+app.post("/user",(req,res)=>{
+    res.send("Data successfully saved to the database!");
+})
+
+
+app.delete("/user",(req,res)=>{
+    res.send("Data sucessfully deleted from the database!");
+});
+
+
+ app.use("/hello/2",(req,res)=>{
+    res.send("Hello,Hello,Hello");
+});
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello from the server");
+});
+ 
+
+
+app.use("/",(req,res)=>{
+    res.send("Namaste Ritesh");
+});
+
+
+
      
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000");
