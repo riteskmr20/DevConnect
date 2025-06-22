b55537c21f3a8d34422ffd9d8be466bee9b2931d
Ritesh kumar <riteshkmr2002@gmail.com>
2025-06-23 04:11:14 +0530

    Error handling in middleware

--- src/app.js
+++ src/app.js
@@ -2,27 +2,23 @@ const express=require("express");
 
 const app=express();
 
-const {adminAuth,userAuth}=require('./middlewares/auth')
-
-//Handle Auth middleware for all GET POST,....requests GET,POST
-app.use("/admin",adminAuth);
-
-
-
-//get understand this userAuth clearly
-app.get("/user",userAuth,(req,res)=>{
-   res.send("User Data sent");
+app.get("/getUserData",(req,res)=>{
+    try{
+      throw new Error("jjghsg");
+      res.send("User data sent");
+    }catch(err){
+      res.status(500).send(err.message);
+    }
+    
 });
 
 
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All Data Sent");
-});
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went wrong!");
+    }
 
-
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("Deleted a User");
-});
+})
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000");
