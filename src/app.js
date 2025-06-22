1542de915b14c395ca65228a4d25bb76b731928f
Ritesh kumar <riteshkmr2002@gmail.com>
2025-06-23 03:11:31 +0530

    handle and check the middleware

--- src/app.js
+++ src/app.js
@@ -4,15 +4,40 @@ const app=express();
 
 
 
+//Handle Auth middleware for all GET POST,....requests GET,POST
+app.use("/admin",(req,res)=>{
+    const token="xyz";
+    const isAdminAuthorized=token==="xyz";
+    if(isAdminAuthorized){
+        res.send("Admin is Authorized");
+    }
+    else{
+        res.status(401).send("Unauthorized request");
+    }
+});
 
- 
-app.get("/user/:userId/:name/:password",(req,res)=>{ 
-    console.log(req.params);
-    res.send({firstname:"Ritesh",lastname:"Kumar"});
+app.get("/admin/getAllData",(req,res)=>{
+    /*const token="xyz";
+    const isAdminAuthorized=token==="xyz";
+    if(isAdminAuthorized){
+        res.send("Admin is Authorized");
+    }
+    else{
+        res.status(401).send("Unauthorized request");
+    }*/
 });
 
 
-      
+app.get("/admin/deleteUser",(req,res)=>{
+    const token="xyz";
+    const isAdminAuthorized=token==="xyz";
+    if(isAdminAuthorized){
+        res.send("Deleted a User");
+    }
+    else{
+        res.status(401).send("Unauthorized request");
+    }
+});
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000");
