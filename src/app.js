9e20d30332ea4b2d19ce3482eb4e4f1052757b60
Ritesh kumar <riteshkmr2002@gmail.com>
2025-06-25 21:04:59 +0530

    user added to the data

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
-const express=require("express");
-const connectDb=require('./config/database');
-const app=express();
+const express = require("express");
+const connectDb = require("./config/database");
+const app = express();
+const User = require("./models/user");
 
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Ritesh",
+    lastName: "Kumar",
+    emailId: "riteshkmr2002@gmail.com",
+    password: "ritesh123",
+  });
+
+  //creating a new user instance of the user model
+  try {
+    await user.save();
+    res.send("User added succesfully!");
+  } catch (err) {
+    res.send("Error while adding to the database:" + err.message);
+  }
+});
 
 connectDb()
-    .then(()=>{
-     console.log("Database connection established...");
-     app.listen(3000,()=>{
-     console.log("Server is successfully listening on port 3000");
-     });
-   })
-   .catch(()=>{
-     console.log("Database cannot be connected!!");
-   });
-
-   
-
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Database cannot be connected!!");
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 
 const connectDb=async()=>{
-    await mongoose.connect('mongodb+srv://ritesh:Ritesh123@cluster0.ww92ok7.mongodb.net/codeTalk');
+    await mongoose.connect("mongodb+srv://ritesh:Ritesh123@cluster0.ww92ok7.mongodb.net/codeTalk");
 };
 
 module.exports=connectDb;
